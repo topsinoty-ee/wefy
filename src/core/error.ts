943404e21bc884f4bd1b94ecc2387c1c1a609886
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-05-08 23:27:31 +0300

    added more props

--- src/core/error.ts
+++ src/core/error.ts
@@ -2,17 +2,29 @@ interface WefyErrorOptions {
   error: unknown;
   cause: unknown;
   status: number;
+  statusText: string;
   response: Response;
 }
 
 export class WefyError extends Error {
-  constructor(message: string, options?: Partial<WefyErrorOptions>) {
+  public response?: Response;
+  public error?: unknown;
+  public status?: number;
+  public statusText?: string;
+  public cause?: unknown;
+
+  constructor(
+    message: string,
+    public readonly options?: Partial<WefyErrorOptions>
+  ) {
     super(message);
     this.name = "WefyError";
+    this.cause = options?.cause;
+    this.status = options?.status;
+    this.statusText = options?.statusText;
+    this.response = options?.response;
+    this.error = options?.error;
 
-    if (options?.cause) {
-      (this as any).cause = options.cause;
-    }
     Object.setPrototypeOf(this, new.target.prototype);
   }
 }
