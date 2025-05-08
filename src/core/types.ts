e746ff28092968fea5b368d34a83d5a9a4ad916c
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-05-08 23:27:18 +0300

    moved types and made better exports

--- src/core/types.ts
+++ src/core/types.ts
@@ -11,3 +11,30 @@ export type Params = Record<
   string,
   string | number | boolean | undefined | (string | number | boolean)[]
 >;
+
+export interface SanitizeUrlOptions {
+  encode?: boolean;
+  preserveEncoding?: boolean;
+}
+
+/**
+ * Configuration interface for Wefy HTTP client
+ */
+export interface WefyConfig extends SanitizeUrlOptions {
+  baseUrl: string;
+  options?: Omit<RequestInit, "method" | "body">;
+  timeout?: number;
+}
+
+/**
+ * Request configuration interface
+ * @template Body - Type of the request body
+ */
+export interface WefyRequestConfig<
+  Body extends BodyInit | object | null | undefined = undefined
+> extends Partial<SanitizeUrlOptions> {
+  params?: Params;
+  options?: RequestInit;
+  body?: Body;
+  timeout?: number;
+}
