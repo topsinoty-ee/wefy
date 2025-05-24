adc8b213be092e115a2315552d3412e47370ed61
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-05-24 11:40:06 +0300

    Add scoped functionality and dynamic methods to Wefy
    Introduced a new `scope` feature enabling dynamic method creation within named scopes, along with associated state management. This enhancement supports better modularity, custom configurations, and encapsulated behavior for HTTP operations within Wefy. Additional methods for managing and inspecting scopes were also added.

--- src/core/wefy.ts
+++ src/core/wefy.ts
@@ -4,26 +4,42 @@ import {WefyParseError, WefyTimeoutError} from "@/core/error.ts";
 import {WefyResponse} from "@/core/response.ts";
 import {produce} from "immer";
 
-type BaseWefy = Omit<Wefy, 'decorate' | 'raw'> & {
+type BaseWefy = Omit<Wefy, 'decorate' | 'raw' | 'scope' | 'request'> & {
   raw: Omit<WefyRaw, 'makeRequest'>;
 };
 
+type ScopeMethod<Args extends unknown[] = unknown[], Return = unknown> = (...args: Args) => Return;
+
 type DecoratedWefy = BaseWefy;
+
+interface ScopeFactoryContext {
+  get: <ResponseData = unknown>(path: string, config?: WefyRequestConfig) => Promise<ResponseData>;
+  post: <ResponseData = unknown, RequestData extends RequestInit['body'] = undefined>(path: string, body?: RequestData, config?: WefyRequestConfig) => Promise<ResponseData>;
+  put: <ResponseData = unknown, RequestData extends RequestInit['body'] = undefined>(path: string, body?: RequestData, config?: WefyRequestConfig) => Promise<ResponseData>;
+  patch: <ResponseData = unknown, RequestData extends RequestInit['body'] = undefined>(path: string, body?: RequestData, config?: WefyRequestConfig) => Promise<ResponseData>;
+  delete: <ResponseData = unknown>(path: string, config?: WefyRequestConfig) => Promise<ResponseData>;
+  decorate: <Config extends Partial<WefyConfig>>(config: Config) => ScopeFactoryContext;
+  state: Map<string, unknown>;
+}
+
+type ScopeInfo<T extends Record<string, ScopeMethod>> = {
+  methods: T; state: Map<string, unknown>; config?: Partial<WefyConfig>;
+};
 
 abstract class HttpMethodsBase<ReturnType = unknown> {
   get<ResponseData = ReturnType>(path: string, config?: WefyRequestConfig): Promise<ResponseData> {
     return this.makeRequest<ResponseData>('GET', path, undefined, config);
   }
   
-  post<ResponseData = ReturnType, RequestData extends RequestInit['body' ] = undefined>(path: string, body?: RequestData, config?: WefyRequestConfig): Promise<ResponseData> {
+  post<ResponseData = ReturnType, RequestData extends RequestInit['body'] = undefined>(path: string, body?: RequestData, config?: WefyRequestConfig): Promise<ResponseData> {
     return this.makeRequest<ResponseData, RequestData>('POST', path, body, config);
   }
   
-  put<ResponseData = ReturnType, RequestData extends RequestInit['body' ] = undefined>(path: string, body?: RequestData, config?: WefyRequestConfig): Promise<ResponseData> {
+  put<ResponseData = ReturnType, RequestData extends RequestInit['body'] = undefined>(path: string, body?: RequestData, config?: WefyRequestConfig): Promise<ResponseData> {
     return this.makeRequest<ResponseData, RequestData>('PUT', path, body, config);
   }
   
-  patch<ResponseData = ReturnType, RequestData extends RequestInit['body' ] = undefined>(path: string, body?: RequestData, config?: WefyRequestConfig): Promise<ResponseData> {
+  patch<ResponseData = ReturnType, RequestData extends RequestInit['body'] = undefined>(path: string, body?: RequestData, config?: WefyRequestConfig): Promise<ResponseData> {
     return this.makeRequest<ResponseData, RequestData>('PATCH', path, body, config);
   }
   
@@ -48,6 +64,7 @@ class WefyRaw extends HttpMethodsBase {
 class Wefy extends HttpMethodsBase {
   public readonly raw: WefyRaw;
   private readonly fetch: typeof fetch;
+  private scopes = new Map<string, ScopeInfo<Record<string, ScopeMethod>>>();
   
   private constructor(private readonly config: WefyConfig) {
     super();
@@ -77,6 +94,7 @@ class Wefy extends HttpMethodsBase {
         encode: config?.encode ?? true, preserveEncoding: config?.preserveEncoding ?? true
       };
       
+      // noinspection HttpUrlsUsage
       const isAbsoluteUrl = path.startsWith("https://") || path.startsWith("http://");
       const url = sanitizeUrl(isAbsoluteUrl ? path : this.config.baseUrl, isAbsoluteUrl ? "" : path, params, sanitizeOptions);
       
@@ -129,6 +147,72 @@ class Wefy extends HttpMethodsBase {
     }) as this & { [K in Name]: DecoratedWefy };
   }
   
+  scope<Name extends string, T extends Record<string, ScopeMethod>>(name: Name, methods: T, config?: Partial<WefyConfig>): this & { [K in Name]: T };
+  
+  scope<Name extends string, T extends Record<string, ScopeMethod>>(name: Name, fn: (ctx: ScopeFactoryContext) => T, config?: Partial<WefyConfig>): this & { [K in Name]: T };
+  
+  scope<Name extends string, T extends Record<string, ScopeMethod>>(name: Name, objOrFn: T | ((ctx: ScopeFactoryContext) => T), config?: Partial<WefyConfig>): this & { [K in Name]: T } {
+    if (this.scopes.has(name)) {
+      throw new Error(`Scope "${name}" already exists.`);
+    }
+    
+    const scopedWefy = config ? Wefy.create({
+      ...this.config, ...config, options: {
+        ...this.config.options, ...config.options, headers: new Headers({
+          ...this.config.options?.headers, ...config.options?.headers,
+        }),
+      },
+    }) : this;
+    
+    const state = new Map<string, unknown>();
+    
+    const createContext = (wefy: Wefy): ScopeFactoryContext => ({
+      get: wefy.get.bind(wefy),
+      post: wefy.post.bind(wefy),
+      put: wefy.put.bind(wefy),
+      patch: wefy.patch.bind(wefy),
+      delete: wefy.delete.bind(wefy),
+      decorate: <DecConfig extends Partial<WefyConfig>>(decorateConfig: DecConfig): ScopeFactoryContext => {
+        const newWefy = Wefy.create({
+          ...wefy.config, ...decorateConfig, options: {
+            ...wefy.config.options, ...decorateConfig.options, headers: new Headers({
+              ...wefy.config.options?.headers, ...decorateConfig.options?.headers,
+            }),
+          },
+        });
+        return createContext(newWefy);
+      },
+      state,
+    });
+    
+    const context = createContext(scopedWefy);
+    
+    const methods = typeof objOrFn === 'function' ? objOrFn(context) : objOrFn;
+    
+    const scopeInfo: ScopeInfo<T> = {methods, state, config};
+    this.scopes.set(name, scopeInfo as ScopeInfo<Record<string, ScopeMethod>>);
+    
+    const newThis = produce(this, (draft) => {
+      Object.defineProperty(draft, name, {
+        value: methods, writable: false, enumerable: true, configurable: false,
+      });
+    });
+    
+    return newThis as this & { [K in Name]: T };
+  }
+  
+  public getScopeInfo<T extends Record<string, ScopeMethod>>(name: string): ScopeInfo<T> | undefined {
+    return this.scopes.get(name) as ScopeInfo<T> | undefined;
+  }
+  
+  public hasScope(name: string): boolean {
+    return this.scopes.has(name);
+  }
+  
+  listScopes(): string[] {
+    return Array.from(this.scopes.keys());
+  }
+  
   protected async makeRequest<ResponseData, RequestData extends RequestInit['body'] = undefined>(method: HttpMethod, path: string, body?: RequestData, config?: WefyRequestConfig): Promise<ResponseData> {
     const response = await this.request<ResponseData, RequestData>(method, path, body, config);
     return (response instanceof WefyResponse ? await response.auto() : response);
@@ -142,8 +226,3 @@ class Wefy extends HttpMethodsBase {
 }
 
 export {Wefy};
-
-const api = Wefy.create("").decorate("test", {})
-console.log(await api.test.get<string>("dfd"));
-
-
